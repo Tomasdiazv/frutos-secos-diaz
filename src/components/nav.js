9d1dc28498d24c3b06e69c7e498923eeea423737
Tomas Diaz <tomas_diaz@live.cl>
2022-06-20 16:30:55 -0400

    se agrega barra de navegacion

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,18 +1,21 @@
 import logo from '../assets/imagenlogo1.jpg'
+import './nav.css'
+/* import carrito from '../assets/carrito.png' */
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const NavBar = () => {
     return (
         <header>
-            <img src={logo} alt="" className='App-logo' />
-            <h1></h1>
-            <nav className='index__navegacion'>
+            <img src={logo} alt=""  />
+            <h1>Frutos Secos</h1>
+            <nav>
                 <a href="">Cat1</a>
                 <a href="">Cat2</a>
                 <a href="">Cat3</a>
                 <a href="">Cat4</a>
             </nav>
-            <ShoppingCartIcon />
+            {/* <img src={carrito} alt="" /> */}
+            < ShoppingCartIcon />
         </header>
     )
 }
